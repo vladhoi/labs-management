23cfe7b354fa962be9ce1807d1138060d42cb7e2
Roman <doroshroman27@gmail.com>
2020-10-08 18:36:29 +0300

    implemented Subject model

---
+++ app/subjects/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+class Subject(models.Model):
+    title = models.CharField(max_length=100, blank=False)
+    description = models.TextField()
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
