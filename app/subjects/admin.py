9693d62174965420e2fcfe78438b5a369866c0f4
Roman <doroshroman27@gmail.com>
2020-10-08 19:12:28 +0300

    updated admin panel view

--- app/subjects/admin.py
+++ app/subjects/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Subject
 
-# Register your models here.
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ["title", "description", "user"]
+    ordering = ["title"]
+
+admin.site.register(Subject, SubjectAdmin)
+
--- app/subjects/models.py
+++ app/subjects/models.py
@@ -6,3 +6,6 @@ class Subject(models.Model):
     title = models.CharField(max_length=100, blank=False)
     description = models.TextField()
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
