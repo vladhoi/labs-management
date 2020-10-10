a22c8086a231c608d12826a61b74608254d7630e
BogVin <spongebob69961@gmail.com>
2020-10-10 23:09:04 +0300

    fix

--- app/core/urls.py
+++ app/core/urls.py
@@ -19,7 +19,7 @@ from users.views import UserList
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('users', UserList)
+router.register("users", UserList)
 
 
 urlpatterns = [
