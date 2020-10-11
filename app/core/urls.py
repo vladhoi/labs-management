976060c8fa0a91f48fc987a81039815ba5a3d736
BogVin <spongebob69961@gmail.com>
2020-10-11 16:07:30 +0300

    fix

--- app/core/urls.py
+++ app/core/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/users/", include("users.urls")),
-    path("api/v1/subjects/", include("subjects.urls")),
+    path("api/v1/subjects/", include("subjects.urls"))
 ]
