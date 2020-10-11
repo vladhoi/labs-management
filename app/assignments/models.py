b5970fb11f0a4481f079e8b8be237adfd7ee9e8e
Vlad Hoi <hoivladyslav@yahoo.com>
2020-10-11 19:58:05 +0300

    Added assignment model.

---
+++ app/assignments/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+class Assignment(models.Model):
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, default='')
+    created_by = models.ForeignKey(get_user_model(),
+                                   on_delete=models.DO_NOTHING,
+                                   related_name='+')
+
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
