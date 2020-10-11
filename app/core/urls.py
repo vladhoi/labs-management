474487b8d4c528bc69c6818285408cfb2978b632
BogVin <spongebob69961@gmail.com>
2020-10-11 15:56:32 +0300

    fix

--- app/core/urls.py
+++ app/core/urls.py
@@ -15,15 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users.views import UserList
-from rest_framework import routers
 
-router = routers.DefaultRouter()
-router.register("users", UserList)
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/", include(router.urls)),
+    path("api/v1/users/", include("users.urls")),
     path("api/v1/subjects/", include("subjects.urls")),
 ]
