2da6a40ca6cc2feb1f075853ef04c9cd1fdee0ab
Nazar Shcherbiy <Chay23@users.noreply.github.com>
2020-10-14 15:56:16 +0300

    Implemented Lectures model

---
+++ app/lectures/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from subjects.models import Subject
+
+
+# Create your models here.
+
+
+class Lecture(models.Model):
+    title = models.CharField(max_length=100, null=True, blank=True)
+    text = models.TextField()
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE())
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE())
+
+    def __str__(self):
+        return self.title
