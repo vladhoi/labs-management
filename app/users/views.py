9475387db411ce61824abebfd55a443c4e2b3510
BogVin <34000503+BogVin@users.noreply.github.com>
2020-10-14 11:16:23 +0300

    Update views.py

--- app/users/views.py
+++ app/users/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import viewsets
