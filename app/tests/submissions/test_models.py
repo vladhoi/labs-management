924fbb7aeb148d65f924962fbd0554ce0fbf4c17
BogVin <34000503+BogVin@users.noreply.github.com>
2020-10-18 15:27:34 +0300

    Update test_models.py

--- app/tests/submissions/test_models.py
+++ app/tests/submissions/test_models.py
@@ -5,7 +5,7 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 
 
 @pytest.mark.django_db
-def test_create_submissins(create_valid_user, create_valid_assigment):
+def test_create_submissions(create_valid_user, create_valid_assigment):
     """
     Ensure we can create a new submission.
     """
