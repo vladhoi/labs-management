f03c21c9f72e2a985192729a07a02982faee7108
Vlad Hoi <uvik@uviks-MacBook-Pro.local>
2020-12-15 23:10:42 +0200

    Added missing id field to the Subject serializer

--- app/subjects/serializers.py
+++ app/subjects/serializers.py
@@ -6,7 +6,7 @@ from rest_framework.validators import UniqueTogetherValidator
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
-        fields = ("title", "description")
+        fields = ("id", "title", "description")
 
         validators = [
             UniqueTogetherValidator(
