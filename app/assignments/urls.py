c647edc4e07425d3fd51fc3afa21bc9d120e0241
BogVin <spongebob69961@gmail.com>
2021-01-01 22:31:38 +0200

    Added endopint to get all assignments by id

--- app/assignments/urls.py
+++ app/assignments/urls.py
@@ -1,6 +1,15 @@
 from rest_framework import routers
-from .views import AssignmentViewList
+from .views import AssignmentViewList, UserAssignmentsViewList
+from django.urls import path
 
 router = routers.DefaultRouter()
 router.register("", AssignmentViewList, basename="assignments")
-urlpatterns = router.urls
+
+urlpatterns = [
+    path(
+        "user/<int:id>/",
+        UserAssignmentsViewList.as_view(),
+        name="user-assignments",
+    ),
+]
+urlpatterns += router.urls
