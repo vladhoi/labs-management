105c12491d93973325156dfd385079dd2545ec6f
BogVin <spongebob69961@gmail.com>
2021-01-05 17:51:38 +0200

    Added endpoint for submissions by assignment

--- app/assignments/views.py
+++ app/assignments/views.py
@@ -4,6 +4,8 @@ from .models import Assignment
 from .serializers import AssignmentSerializer
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
+from submissions.models import Submission
+from submissions.serializers import SubmissionSerializer
 
 
 class AssignmentViewList(viewsets.ModelViewSet):
@@ -20,6 +22,20 @@ class UserAssignmentsViewList(ListAPIView):
     def get(self, request, *args, **kwargs):
         user_id = kwargs.get("id", "")
         queryset = Assignment.objects.filter(created_by=user_id)
+        serializer = self.get_serializer(queryset, many=True)
+
+        return Response(serializer.data)
+
+
+class AssignmentSubmissionsViewList(ListAPIView):
+    serializer_class = SubmissionSerializer
+
+    def get_queryset(self):
+        pass
+
+    def get(self, request, *args, **kwargs):
+        assignment_id = kwargs.get("id", "")
+        queryset = Submission.objects.filter(assignment=assignment_id)
         serializer = self.get_serializer(queryset, many=True)
 
         return Response(serializer.data)
